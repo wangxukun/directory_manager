ca2b7b9241bd625388d5d82e3bcb1de3a665522e
wangxk-office <wang_xukun@163.com>
2024-04-09 10:17:45 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ console.log(figlet.textSync("Dir Manager"));
 
 program
     .version("1.0.0")
+    .name("dirmanager")
+    .usage('[options]')
     .description("An example CLI for managing a directory")
     .option("-l, --ls  [value]", "List directory contents")
     .option("-m, --mkdir <value>", "Create a directory")
